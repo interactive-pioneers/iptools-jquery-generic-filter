282fb6830a4219e8eafe3df21baaead9874d212b
Georg Meyer <gm@interactive-pioneers.de>
2015-07-13 17:32:48 +0200

    Add tests for init and destroy

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,9 +1,36 @@
 /* jshint undef: false */
-
 (function() {
   'use strict';
 
   describe('IPTGenericFilter', function() {
+    var config = {};
+    var pluginName = 'plugin_iptGenericFilter';
+    var $object = null;
+
+    describe('init', function() {
+
+      beforeEach(function() {
+        $object = $('#level-0').iptGenericFilter(config);
+      });
+
+      it('expected to construct object', function() {
+        return expect($object).to.be.an.object;
+      });
+
+    });
+
+    describe('destroy', function() {
+
+      beforeEach(function() {
+        $object = $('#level-0').iptGenericFilter(config);
+      });
+
+      it('expected to remove data', function() {
+        $object.data(pluginName).destroy();
+        return expect($object.data(pluginName)).to.not.be.ok;
+      });
+
+    });
   });
 
 })();
