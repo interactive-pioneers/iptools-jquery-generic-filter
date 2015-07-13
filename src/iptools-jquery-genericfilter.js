34f0c06662c447dfa9960e15f0fbf61da6039938
Georg Meyer <gm@interactive-pioneers.de>
2015-07-13 16:10:04 +0200

    Add support for selects

--- src/iptools-jquery-genericfilter.js
+++ src/iptools-jquery-genericfilter.js
@@ -1,11 +1,16 @@
 /* globals jQuery */
-;(function($) {
+(function($) {
 
   'use strict';
 
   var pluginName = 'iptGenericFilter';
 
-  var defaults = {};
+  var defaults = {
+    child: null
+  };
+
+  // mooc data @TODO
+  var JSONMoocData = '{"0": "please choose a child", "1": "option 1", "2": "option 2", "3": "option 3"}';
 
   function IPTGenericFilter(element, options) {
     this.$element = $(element);
@@ -19,15 +24,68 @@
   IPTGenericFilter.prototype = {
 
     init: function() {
+      // bail early if config is invalid
+      if (this.settings.child === null) {
+        this.destroy();
+        return;
+      }
+
+      // make sure child has attr disabled set
+      $('*[name="' + this.settings.child + '"]').attr('disabled', 'disabled');
+
       this.addEventListeners();
     },
 
+    handleChange: function(event) {
+      var self = event.data;
+      var filter = self.$element.attr('name');
+      var value = self.getValue(self);
+      console.log('value ' + value);
+      if (value !== 0) {
+        self.fetchData(filter, value);
+      } else {
+        self.updateChild(null);
+      }
+    },
+
+    updateChild: function(data) {
+      var child = $('*[name="' + this.settings.child + '"]');
+      var $option = null;
+      console.log('updateChild ' + this.settings.child + ' length: ' + child.length + ' data: ' + data);
+
+      child.empty();
+
+      if (null !== data) {
+        $.each(data, function(value, label) {
+          $option = $('<option value="' + value + '">' + label + '</option>');
+          child.append($option);
+        });
+        child.removeAttr('disabled');
+      } else {
+        child.attr('disabled', 'disabled');
+      }
+      child.val(0);
+      child.trigger('change');
+    },
+
+    getValue: function(self) {
+      var value = $.trim(self.$element.val());
+      value = parseInt(value, 10) || 0;
+      return value;
+    },
+
+    fetchData: function(filter, value) {
+      // implement ajax functionality here
+      var data = $.parseJSON(JSONMoocData);
+      this.updateChild(data, filter, value);
+    },
+
     addEventListeners: function() {
-      // ...
+      this.$element.on('change' + '.' + this.name, null, this, this.handleChange);
     },
 
     destroy: function() {
-      //this.$element.off('event' + '.' + this._name);
+      this.$element.off('change' + '.' + this._name);
       this.$element.removeData('plugin_' + pluginName);
     }
 
