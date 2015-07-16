1b0cf845d6b349510b559ba435672b284c32613f
Georg Meyer <gm@interactive-pioneers.de>
2015-07-16 21:45:39 +0200

    Refactor test scenario, Fix #2

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,14 +3,22 @@
   'use strict';
 
   describe('IPTGenericFilter', function() {
-    var config = {};
+
+    var config = {
+      child: 'level-1'
+    };
     var pluginName = 'plugin_iptGenericFilter';
+    var selector = '#level-0';
     var $object = null;
 
     describe('init', function() {
 
       beforeEach(function() {
-        $object = $('#level-0').iptGenericFilter(config);
+        $object = $(selector).iptGenericFilter(config);
+      });
+
+      afterEach(function() {
+        $object.data(pluginName).destroy();
       });
 
       it('expected to construct object', function() {
@@ -22,7 +30,7 @@
     describe('destroy', function() {
 
       beforeEach(function() {
-        $object = $('#level-0').iptGenericFilter(config);
+        $object = $(selector).iptGenericFilter(config);
       });
 
       it('expected to remove data', function() {
